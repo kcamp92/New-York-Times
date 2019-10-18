7505e3e756c610610f4e4f2e22df8b1dd7fdb9d6
Krystal Campbell <krystalcampbell@Krystals-MacBook-Pro.local>
2019-10-18 14:34:34 -0400

    add gitignore file

---
+++ NewYorkTimes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewYorkTimes
+//
+//  Created by Krystal Campbell on 10/18/19.
+//  Copyright © 2019 Krystal Campbell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
