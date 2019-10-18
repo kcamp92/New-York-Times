e3a4696bf0d31299a75c34e9784f4a978bdb289f
phoenixmcknight <phoenixmcknight@gmail.com>
2019-10-18 17:55:44 -0400

    adds NYTAPIClient and test

--- NewYorkTimes/Networking/GoogleBooksAPI.swift
+++ NewYorkTimes/Networking/GoogleBooksAPI.swift
@@ -8,3 +8,27 @@
 
 import Foundation
 
+class GoogleBookAPI {
+    static let shared = GoogleBookAPI()
+    
+    func getGoogleBookData(isbn10:String,completionHandler:@escaping(Result<Books?,AppError>)-> Void) {
+        let url = "https://www.googleapis.com/books/v1/volumes?q=+isbn:\(isbn10)&key=AIzaSyBQ_TfDLtpJUwd6ZPumogW6eREP3VW5PKw"
+        
+        guard let urlStr = URL(string: url) else {
+            return
+        }
+
+        NetworkHelper.manager.performDataTask(withUrl: urlStr, andMethod: .get) { (results) in
+            switch results {
+            case .failure(let error):
+                completionHandler(.failure(error))
+            case .success(let data):
+                do {  let bookData = try JSONDecoder().decode(Items.self, from: data)
+                    completionHandler(.success(bookData.volumeInfo))
+                } catch {
+                    completionHandler(.failure(.badURL))
+                }
+        }
+    }
+}
+}
